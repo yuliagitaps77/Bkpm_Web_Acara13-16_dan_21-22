ce1ff9361f8378aed085c8a5bce0b327f2e6cd6b
yuliagitaps77 <gita.permata.saryy@gmail.com>
2025-03-21 08:59:09 +0700

    add api pendidikan controller

---
+++ app/Http/Controllers/Backend/ApiPendidikanController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pendidikan;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+
+class ApiPendidikanController extends Controller
+{
+    // Mengambil semua data Pendidikan
+    public function getAll()
+    {
+        $pendidikan = Pendidikan::all();
+        return response()->json($pendidikan, 200);
+    }
+
+    // Membuat data Pendidikan baru
+    public function create(Request $request)
+    {
+        $pendidikan = Pendidikan::create($request->all());
+        return response()->json($pendidikan, 201);
+    }
+
+    public function getPen($id)
+    {
+        $pendidikan = Pendidikan::find($id);
+        return Response::json($pendidikan, 200);
+    }
+
+    public function createPen(Request $request)
+    {
+        Pendidikan::create($request->all());
+
+        return response()->json([
+            'status' => 'ok',
+            'message' => 'Pendidikan berhasil ditambahkan!'
+        ], 201);
+    }
+
+    public function updatePen($id, Request $request)
+    {
+        Pendidikan::find($id)->update($request->all());
+
+        return response()->json([
+            'status' => 'ok',
+            'message' => 'Pendidikan berhasil dirubah!'
+        ], 201);
+    }
+
+    public function deletePen($id)
+    {
+        Pendidikan::destroy($id);
+
+        return response()->json([
+            'status' => 'ok',
+            'message' => 'Pendidikan berhasil dihapus!'
+        ], 201);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\DashboardController;
 use App\Http\Controllers\Backend\PendidikanController;
 use App\Http\Controllers\Backend\PengalamanKerjaController;
+use App\Http\Controllers\Backend\ApiPendidikanController;
 
 
 Route::resource('/dashboard', DashboardController::class);
@@ -16,4 +17,12 @@ Route::prefix('backend')->group(function () {
  Route::get('/pengalaman-kerja/search', [PengalamanKerjaController::class, 'search'])->name('pengalaman_kerja.search');
 
  
+ Route::controller(ApiPendidikanController::class)->group(function () {
+    Route::get('api_pendidikan', 'getAll');
+    Route::get('api_pendidikan/{id}', 'getPenById');
+    Route::post('api_pendidikan', 'create');
+    Route::put('api_pendidikan/{id}', 'update');
+    Route::delete('api_pendidikan/{id}', 'deletePen');
+});
+
 });
